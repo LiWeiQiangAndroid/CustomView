2a4c904f80d551f0baacec7b9e0807e81eaaed76
LiWeiQiang <lwqldsyzx@126.com>
2016-10-04 23:24:38 +0800

    修改

--- app/src/main/java/com/lwq/android/customview/view/CustomTitleView.java
+++ app/src/main/java/com/lwq/android/customview/view/CustomTitleView.java
@@ -22,15 +22,16 @@ import java.util.Set;
  * Description ：
  */
 
-public class CustomTitleView extends View {
+public class CustomTitleView extends View
+{
     /**
      * 文本
      */
     private String mTitleText;
     /**
-     * 文本颜色
+     * 文本的颜色
      */
-    private  int mTitleTextColor;
+    private int mTitleTextColor;
     /**
      * 文本的大小
      */
@@ -42,77 +43,99 @@ public class CustomTitleView extends View {
     private Rect mBound;
     private Paint mPaint;
 
-    /**
-     *
-     * @param context
-     */
-    public CustomTitleView(Context context) {
-        super(context,null);
+    public CustomTitleView(Context context, AttributeSet attrs)
+    {
+        this(context, attrs, 0);
+    }
+
+    public CustomTitleView(Context context)
+    {
+        this(context, null);
     }
 
     /**
-     *获得我自定义的样式属性
+     * 获得我自定义的样式属性
+     *
      * @param context
      * @param attrs
-     * @param defStyleAttr
+     * @param defStyle
      */
-    public CustomTitleView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+    public CustomTitleView(Context context, AttributeSet attrs, int defStyle)
+    {
+        super(context, attrs, defStyle);
         /**
          * 获得我们所定义的自定义样式属性
          */
-        TypedArray a=context.getTheme().obtainStyledAttributes(attrs, R.styleable.CustomTitleView,defStyleAttr,0);
-        int n=a.getIndexCount();
-        for (int i=0;i<n;i++){
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.CustomTitleView, defStyle, 0);
+        int n = a.getIndexCount();
+        for (int i = 0; i < n; i++)
+        {
             int attr = a.getIndex(i);
-            switch (attr){
+            switch (attr)
+            {
                 case R.styleable.CustomTitleView_titleText:
                     mTitleText = a.getString(attr);
                     break;
                 case R.styleable.CustomTitleView_titleTextColor:
-                    //默认设置为黑色
-                    mTitleTextColor=a.getColor(attr, Color.BLACK);
+                    // 默认颜色设置为黑色
+                    mTitleTextColor = a.getColor(attr, Color.BLACK);
                     break;
                 case R.styleable.CustomTitleView_titleTextSize:
-                    mTitleTextSize = a.getDimensionPixelSize(attr, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,16,getResources().getDisplayMetrics()));
+                    // 默认设置为16sp，TypeValue也可以把sp转化为px
+                    mTitleTextSize = a.getDimensionPixelSize(attr, (int) TypedValue.applyDimension(
+                            TypedValue.COMPLEX_UNIT_SP, 16, getResources().getDisplayMetrics()));
                     break;
+
             }
 
         }
         a.recycle();
+
         /**
          * 获得绘制文本的宽和高
          */
-        mPaint =new Paint();
+        mPaint = new Paint();
         mPaint.setTextSize(mTitleTextSize);
-        mBound =new Rect();
-        mPaint.getTextBounds(mTitleText,0,mTitleText.length(),mBound);
-        this.setOnClickListener(new OnClickListener() {
+        // mPaint.setColor(mTitleTextColor);
+        mBound = new Rect();
+        mPaint.getTextBounds(mTitleText, 0, mTitleText.length(), mBound);
+
+        this.setOnClickListener(new OnClickListener()
+        {
+
             @Override
-            public void onClick(View v) {
-                mTitleText=randomText();
+            public void onClick(View v)
+            {
+                mTitleText = randomText();
                 postInvalidate();
             }
-        });
-    }
 
-    private String randomText() {
+        });
+
+    }
+    private String randomText()
+    {
         Random random = new Random();
         Set<Integer> set = new HashSet<Integer>();
-        while (set.size()<4){
-            int randomInt=random.nextInt(10);
+        while (set.size() < 4)
+        {
+            int randomInt = random.nextInt(10);
             set.add(randomInt);
         }
         StringBuffer sb = new StringBuffer();
-        for (Integer i :set){
-            sb.append(""+i);
+        for (Integer i : set)
+        {
+            sb.append("" + i);
         }
-        return  sb.toString();
+
+        return sb.toString();
     }
 
     @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        //super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+        // super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
         int width = 0;
         int height = 0;
 
@@ -121,36 +144,42 @@ public class CustomTitleView extends View {
          */
         int specMode = MeasureSpec.getMode(widthMeasureSpec);
         int specSize = MeasureSpec.getSize(widthMeasureSpec);
-        switch (specMode) {
-            case MeasureSpec.EXACTLY://明确指定了
+        switch (specMode)
+        {
+            case MeasureSpec.EXACTLY:// 明确指定了
                 width = getPaddingLeft() + getPaddingRight() + specSize;
                 break;
-            case MeasureSpec.AT_MOST://一般为WARP_CONTENT
+            case MeasureSpec.AT_MOST:// 一般为WARP_CONTENT
                 width = getPaddingLeft() + getPaddingRight() + mBound.width();
                 break;
         }
+
         /**
          * 设置高度
          */
         specMode = MeasureSpec.getMode(heightMeasureSpec);
         specSize = MeasureSpec.getSize(heightMeasureSpec);
-        switch (specMode) {
-            case MeasureSpec.EXACTLY://明确指定了
+        switch (specMode)
+        {
+            case MeasureSpec.EXACTLY:// 明确指定了
                 height = getPaddingTop() + getPaddingBottom() + specSize;
                 break;
-            case MeasureSpec.AT_MOST://一般为WARP_CONTENT
-                height = getPaddingTop() + getPaddingBottom() + mBound.width();
+            case MeasureSpec.AT_MOST:// 一般为WARP_CONTENT
+                height = getPaddingTop() + getPaddingBottom() + mBound.height();
                 break;
         }
 
-        setMeasuredDimension(width,height);
+        setMeasuredDimension(width, height);
+
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
+    protected void onDraw(Canvas canvas)
+    {
         mPaint.setColor(Color.YELLOW);
-        canvas.drawRect(0,0,getMeasuredWidth(),getMeasuredHeight(),mPaint);
+        canvas.drawRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), mPaint);
+
         mPaint.setColor(mTitleTextColor);
-        canvas.drawText(mTitleText,getWidth()/2-mBound.width()/2,getHeight()/2+mBound.height(),mPaint);
+        canvas.drawText(mTitleText, getWidth() / 2 - mBound.width() / 2, getHeight() / 2 + mBound.height() / 2, mPaint);
     }
 }
